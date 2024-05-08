821e84cb20d62c69ca4aee007ac26269272f3b2a
Mystic Cloud <myssytys@gmail.com>
2024-05-08 22:18:24 +0300

    Add files via upload

--- Cube.h
+++ Cube.h
@@ -1,15 +1,26 @@
 #ifndef CUBE_H
 #define CUBE_H
 
+#include <glm/glm.hpp>
+#include <vector>
+
 class Cube {
     
     public:
 
-    float* vertices;
-    int* indices;
+    struct Vertex {
+        glm::vec3 pos;
+        glm::vec4 colors;
+        glm::vec2 texCoords;
+    };
+    
+    //glm::vec3 vertices;
+    std::vector<int> indices;
+    std::vector<Vertex> Data;
+    //glm::vec4 colors;
+    //glm::vec2 texCoords;
     Cube();
     ~Cube();
     
 };
-
 #endif
